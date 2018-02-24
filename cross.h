03b2ecebd8672dd5fbd4e31af00d94f15b2f5f68
James Milne <s4b3r6@gmail.com>
2018-02-25 10:56:45 +1100

    FIX: Ubuntu has issues with usleep, so let's use nanosleep, its replacement.

--- cross.h
+++ cross.h
@@ -4,7 +4,7 @@
 #if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
 #include <Windows.h>
 #else
-#include <unistd.h>
+#include <time.h>
 #endif
 
 void clearscr(void);
@@ -24,7 +24,10 @@ void sleepn(int millis) {
   #if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
   Sleep(millis);
   #else
-  usleep((useconds_t)millis*1000);
+  struct timespec timer;
+  timer.tv_sec = millis/1000;
+  timer.tv_nsec = 0;
+  nanosleep(&timer, NULL);
   #endif
 }
 
